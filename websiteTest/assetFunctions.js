ed90b1f9229387aef060d963a26a45fe590af5d2
Stephen Rhoades <stephenrhoades04@gmail.com>
2025-02-04 14:53:48 +0000

    Removed embedded repositories and updated .gitignore

---
+++ websiteTest/assetFunctions.js
@@ -0,0 +1,7 @@
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('header').addEventListener('click', function() {
+        alert('Header clicked!');
+    });
+});
